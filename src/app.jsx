015c8a165b01feff68d30a4c038f4202848cda66
Alessandro Lepri <alex.lepri84@gmail.com>
2020-06-04 11:29:53 +0100

    up & running

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import './module.scss'
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+    };
+
+    render() {
+        return <h1>Hello World</h1>
+    };
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require ('path');
+const webpack = require ('webpack');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const Dotenv = require ('dotenv-webpack');
+
+const fs = require ('fs');
+const appDirectory = fs.realpathSync (process.cwd ());
+const resolveApp = relativePath => path.resolve (appDirectory, relativePath);
+
+module.exports = {
+  entry: resolveApp ('src/app.jsx'),
+  output: {
+    path: resolveApp ('dist'),
+    filename: 'bundle.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devtool: 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: ['file-loader'],
+      },
+      {
+        // look for .js or .jsx files
+        test: /\.(js|jsx)$/,
+        // in the `src` directory
+        include: resolveApp ('src'),
+        exclude: /(node_modules)/,
+        use: {
+          // use babel for transpiling JavaScript files
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: {auto: true},
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve ('src'),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        secure: false,
+      },
+    },
+  },
+  plugins: [
+    new Dotenv (),
+    new webpack.HotModuleReplacementPlugin (),
+    new HtmlWebpackPlugin ({
+      template: __dirname + '/src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  resolve: {
+    // File extensions. Add others and needed (e.g. scss, json)
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules'],
+    // Aliases help with shortening relative paths
+    // 'Components/button' === '../../../components/button'
+    alias: {
+      Components: path.resolve (resolveApp ('src'), 'components'),
+      Containers: path.resolve (resolveApp ('src'), 'containers'),
+      Utils: path.resolve (resolveApp ('src'), 'utils'),
+    },
+  },
+};

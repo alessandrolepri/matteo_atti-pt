78cfd418ff3400df8e416c92ffe9fe5d0a953384
Alessandro Lepri <alex.lepri84@gmail.com>
2020-06-12 16:58:07 +0100

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(express.static(`${__dirname}/dist`));
 
 app.use(bodyParser.json());
 
-app.get("/*", (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
+// app.get("/*", (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
+app.get("*", function(req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
+});
 
 app.listen(process.env.PORT || 4000, () =>
     console.log(`Up and running on port ${process.env.PORT}`)

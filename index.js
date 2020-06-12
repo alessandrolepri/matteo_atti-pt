70ac4f6d9b554cb1def92c9b0bffef420fb90e37
Alessandro Lepri <alex.lepri84@gmail.com>
2020-06-12 17:01:41 +0100

    reverted index.js

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ app.use(express.static(`${__dirname}/dist`));
 
 app.use(bodyParser.json());
 
-// app.get("/*", (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
-app.get("*", function(req, res) {
-  const index = path.join(__dirname, "build", "index.html");
-  res.sendFile(index);
-});
+app.get("/*", (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
 
 app.listen(process.env.PORT || 4000, () =>
     console.log(`Up and running on port ${process.env.PORT}`)
